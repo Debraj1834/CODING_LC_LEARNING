fc5ac3584fe763d40301afb7adb597958fdb0eec
Debraj Ray <debraj.10709135@ltimindtree.com>
2024-02-19 12:00:52 +0530

    added few documents

--- hello.py
+++
@@ -1,8 +0,0 @@
-x = []
-for i in range(10):
-    if i<=1:
-        x.append(i)
-    else:
-        x.append(x[i-1]+x[i-2])
-
-print("Fibonacci series: ", x)
---
+++ hello2.py
@@ -0,0 +1,1 @@
+import py_modules.hello as hello
